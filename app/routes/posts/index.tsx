312b3c817356f1cb950b30d7eba4e6ea3652bd47
Dustin Goodman <dustin.s.goodman@gmail.com>
2022-01-01 13:32:52 -0600

    Loading Data: Add the Post type and generic for useLoaderData

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,7 +1,12 @@
 import { Link, useLoaderData } from "remix";
 
+export type Post = {
+  slug: string;
+  title: string;
+};
+
 export const loader = () => {
-  return [
+  const posts: Post[] = [
     {
       slug: "my-first-post",
       title: "My First Post"
@@ -11,10 +16,11 @@ export const loader = () => {
       title: "A Mixtape I Made Just For You"
     }
   ];
+  return posts;
 };
 
 export default function Posts() {
-  const posts = useLoaderData();
+  const posts = useLoaderData<Post[]>();
   return (
     <div>
       <h1>Posts</h1>
