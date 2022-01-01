a2d65046d5ae847bd0b7bb83263850a8f82aecbf
Dustin Goodman <dustin.s.goodman@gmail.com>
2022-01-01 17:57:32 -0600

    Actions: add create post form

--- app/routes/admin/new.tsx
+++ app/routes/admin/new.tsx
@@ -1,3 +1,32 @@
+import { Form } from "remix";
+
 export default function NewPost() {
-  return <h2>New Post</h2>;
+  return (
+    <Form method="post">
+      <p>
+        <label htmlFor="title">
+          Post Title:
+          &nbsp;
+          <input type="text" name="title" id="title" />
+        </label>
+      </p>
+      <p>
+        <label htmlFor="slug">
+          Post Slug:
+          &nbsp;
+          <input type="text" name="slug" id="slug" />
+        </label>
+      </p>
+      <p>
+        <label htmlFor="markdown">
+          Markdown:
+        </label>
+        <br />
+        <textarea name="markdown" id="markdown" rows={20} cols={100} />
+      </p>
+      <p>
+        <button type="submit">Create Post</button>
+      </p>
+    </Form>
+  );
 }
